2cca40f5b98ae9091948aad1d5a4f069a848c8f4
charskiy <mtovkay@gmail.com>
2024-05-22 22:26:38 +0200

    second

--- src/console/PlayRoom.java
+++ src/console/PlayRoom.java
@@ -1,5 +1,8 @@
 package console;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 enum Brand {
     SONY,
     MICROSOFT,
@@ -29,6 +32,40 @@ public class PlayRoom {
             console.loadGame(game.getData());
             console.playGame();
         }
+
+
+
+
+        /*
+        Game.GameDisk gameDisk1 = new Game.GameDisk("Game1", Game.Genre.ACTION, "Description1") {};
+        Game.GameDisk gameDisk2 = new Game.GameDisk("Game2", Game.Genre.SPORT, "Description2") {};
+        Game.GameDisk gameDisk3 = new Game.GameDisk("Game3", Game.Genre.RACE, "Description3") {};
+        Game.GameDisk gameDisk4 = new Game.GameDisk("Game4", Game.Genre.ACTION, "Description4") {};
+
+        Game.GameDisk[] physicalGames = {gameDisk1, gameDisk2, gameDisk3, gameDisk4};
+
+        Arrays.sort(physicalGames, Comparator.comparing(game -> game.getDescription()));
+
+        System.out.println("Physical games sorted by genre:");
+        for (Game.GameDisk game : physicalGames) {
+            System.out.println(game.getData().getName());
+        }
+
+        Game.VirtualGame virtualGame1 = new Game.VirtualGame("VirtualGame1", Game.Genre.ACTION, 4) {};
+        Game.VirtualGame virtualGame2 = new Game.VirtualGame("VirtualGame2", Game.Genre.SPORT, 3) {};
+        Game.VirtualGame virtualGame3 = new Game.VirtualGame("VirtualGame3", Game.Genre.RACE, 5) {};
+        Game.VirtualGame virtualGame4 = new Game.VirtualGame("VirtualGame4", Game.Genre.ACTION, 2) {};
+
+        Game.VirtualGame[] virtualGames = {virtualGame1, virtualGame2, virtualGame3, virtualGame4};
+
+        Arrays.sort(virtualGames, (o1, o2) -> Integer.compare(o1.getRating(), o2.getRating()));
+
+        System.out.println("\nVirtual games sorted by rating:");
+        for (Game.VirtualGame game : virtualGames) {
+            System.out.println(game.getData().getName() + " - Rating: " + game.getRating());
+        }
+
+         */
     }
 }
 
